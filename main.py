e5c9bd1f99e668164377709449b5a0c4d84146db
reidogadobot <79222246+reidogadobot@users.noreply.github.com>
2023-10-07 08:51:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,22 @@
 import os
-import threading
-from flask import Flask, request, jsonify,send_from_directory
+import cv2
+import io
+import tempfile
+from flask import Flask, request, jsonify, send_file
 import imageio
-import tempfile
-import atexit
-import schedule
-import tempfile
-import time
-import uuid
 import shutil
 
 app = Flask(__name__)
 
-@app.route('/')
-def enviar_mensagem():
-    mensagem = "Olá, esta é a rota '/' enviando uma mensagem!"
-    return mensagem
-
 # Caminho para o diretório temporário onde os arquivos serão salvos
 diretorio_temporario = "temp"
 
-# Limpa a pasta temporária à meia-noite
-def limpar_temporario():
-    for arquivo in os.listdir(diretorio_temporario):
-        arquivo_path = os.path.join(diretorio_temporario, arquivo)
-        try:
-            if os.path.isfile(arquivo_path):
-                os.unlink(arquivo_path)
-            elif os.path.isdir(arquivo_path):
-                # Se for um diretório, exclua-o recursivamente
-                for root, dirs, files in os.walk(arquivo_path):
-                    for arquivo_dir in files:
-                        os.unlink(os.path.join(root, arquivo_dir))
-                    for dir_path in dirs:
-                        os.rmdir(os.path.join(root, dir_path))
-                os.rmdir(arquivo_path)
-        except Exception as e:
-            print(f"Erro ao excluir arquivo/diretório {arquivo_path}: {e}")
+# Função para criar um diretório temporário único para cada solicitação
+def criar_diretorio_temporario():
+    return os.path.join(diretorio_temporario)
 
-# Agenda a limpeza da pasta temporária à meia-noite
-schedule.every().day.at("09:13").do(limpar_temporario)
-
-# Função para iniciar a programação de tarefas
-def run_scheduler():
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
-        
 @app.route('/converter', methods=['POST'])
 def converter_video():
-    nome_arquivo = str(uuid.uuid4())[:8]
-# Caminho completo para o arquivo de saída
-    video_saida = os.path.join(diretorio_temporario, f'{nome_arquivo}.mp4')
     try:
         # Verifica se o arquivo 'video' foi enviado na requisição POST
         if 'video' not in request.files:
@@ -59,61 +24,56 @@ def converter_video():
 
         video_file = request.files['video']
 
-        # Verifica a extensão do arquivo (deve ser .webp)
-        if not video_file.filename.endswith('.webp'):
-            return jsonify({'error': 'O arquivo deve ter a extensão .webp'}), 400
+        # Lê o arquivo WebP em um buffer
+        video_buffer = video_file.read()
 
-        # Cria o diretório onde os quadros serão salvos como imagens PNG
-        diretorio_frames = os.path.join(diretorio_temporario, 'frames')
-        os.makedirs(diretorio_frames, exist_ok=True)
+        # Cria um diretório temporário único para esta solicitação
+        diretorio_temporario_atual = criar_diretorio_temporario()
+        os.makedirs(diretorio_temporario_atual)
 
-        # Salva o arquivo de vídeo no diretório temporário
-        video_path = os.path.join(diretorio_temporario, f'{nome_arquivo}.webp')
-        video_file.save(video_path)
+        # Caminho para o arquivo de entrada no formato WebP
+        video_path = os.path.join(diretorio_temporario_atual, 'video.webp')
 
-        # Lê o arquivo de vídeo e extrai os quadros
+        # Salva o buffer no arquivo de entrada
+        with open(video_path, 'wb') as video_file:
+            video_file.write(video_buffer)
+
+        # Caminho para o arquivo de saída em MP4
+        video_saida_path = os.path.join(diretorio_temporario_atual, 'video.mp4')
+
+        # Lê o arquivo WebP e extrai os quadros como imagens PNG
         reader = imageio.get_reader(video_path)
-        frame_count = 0
-        for frame in reader:
-            frame_file = os.path.join(diretorio_frames, f'frame_{nome_arquivo}{frame_count:04d}.png')
-            imageio.imwrite(frame_file, frame)
-            frame_count += 1
+        frames = [frame for frame in reader]
 
-        # Fecha o leitor
-        reader.close()
+        # Obtém as dimensões dos quadros
+        height, width, layers = frames[0].shape
 
-        # Obtém as dimensões de um dos quadros (todos os quadros devem ter o mesmo tamanho)
-        frame = imageio.imread(os.path.join(diretorio_frames, f'frame_{nome_arquivo}0000.png'))
-        height, width, layers = frame.shape
+        # Configura o objeto de gravação de vídeo
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        video_writer = cv2.VideoWriter(video_saida_path, fourcc, 30, (width, height))
 
-        # Cria o escritor de vídeo
-        writer = imageio.get_writer(video_saida, fps=30)
+        # Escreve os quadros no arquivo de vídeo
+        for frame in frames:
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            video_writer.write(frame_rgb)
 
-        # Adiciona cada quadro ao vídeo
-        for i in range(frame_count):
-            frame_file = os.path.join(diretorio_frames, f'frame_{nome_arquivo}{i:04d}.png')
-            frame = imageio.imread(frame_file)
-            writer.append_data(frame)
+        # Fecha o objeto de gravação de vídeo
+        video_writer.release()
 
-        # Fecha o escritor de vídeo
-        writer.close()
+        # Lê o arquivo MP4 em bytes
+        with open(video_saida_path, 'rb') as mp4_file:
+            mp4_data = mp4_file.read()
 
-        # Remove o diretório e todo o seu conteúdo
-        shutil.rmtree(diretorio_frames, ignore_errors=True)
+        # Retorna o arquivo MP4 como resposta
+        response = send_file(io.BytesIO(mp4_data), as_attachment=True, download_name='video.mp4')
 
-        # Retorna o caminho do arquivo de vídeo de saída
-        return jsonify({'video_saida': f"download/{nome_arquivo}.mp4"}), 200
+        # Remove o diretório temporário e todos os seus conteúdos
+        shutil.rmtree(diretorio_temporario_atual, ignore_errors=True)
+
+        return response, 200
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-@app.route('/download/<path:filename>')
-def download_file(filename):
-    # Garante que o caminho absoluto seja usado para evitar problemas
-    diretorio_completo = os.path.join(os.getcwd(), diretorio_temporario)
-    return send_from_directory(diretorio_completo, filename)
 if __name__ == '__main__':
-    scheduler_thread = threading.Thread(target=run_scheduler)
-    scheduler_thread.start()
-
     app.run()
